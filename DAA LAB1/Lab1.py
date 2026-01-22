822498fa6990d69bb812a3ea6055cb7eedddc918
Prathyaksh123 <163908531+Prathyaksh123@users.noreply.github.com>
2026-01-22 08:00:58 +0530

    DAA lab1

---
+++ DAA LAB1/Lab1.py
@@ -0,0 +1,39 @@
+'''write a program to check if the number is even or odd'''
+'''num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")'''
+#-----------------------------------------------------------------------------------------------------------------------
+'''def square(num):
+    return num * num
+
+# Example usage
+result = square(5)
+print(f"Square of 5 is {result}")  # Output: 25'''
+#-----------------------------------------------------------------------------------------------------------------------
+'''num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+if num1 >= num2 and num1 >= num3:
+    print(f"{num1} is the largest.")
+elif num2 >= num1 and num2 >= num3:
+    print(f"{num2} is the largest.")
+else:
+    print(f"{num3} is the largest.")'''
+#-----------------------------------------------------------------------------------------------------------------------
+'''numbers = [1, 2, 3, 4, 5]
+total = sum(numbers)
+print(f"Sum is {total}")  # Output: 15'''
+#-----------------------------------------------------------------------------------------------------------------------
+'''a="hello"
+for i in range(len(a)-1,-1,-1):
+    print(a[i])'''
+#-----------------------------------------------------------------------------------------------------------------------
+a=str(input("enter a word: "))
+if a==a[::-1]:
+    print("the given word is a palindrome")
+else:
+    print("the given word is not a palindrome")
+#-----------------------------------------------------------------------------------------------------------------------
